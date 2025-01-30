7d9e34d09b87925255214b352fd4d33e671aa86b
Rishabh Yadav <yrishabh325@gmail.com>
2025-01-30 20:57:21 +0530

    eleventh commit

--- server/api/index.js
+++ server/api/index.js
@@ -10,14 +10,14 @@ const errorMiddleware = require("./middlewares/error-middleware")
 const PORT = 5000;
 
 // let's Handle Cors Policy issue
-const corsOptions = {
-    origin:"http://localhost:5173",
-    methods:"GET, POST , PUT , DELETE , PATCH , HEAD",
-    credentials:true,
-}
+// const corsOptions = {
+//     origin:"http://localhost:5173",
+//     methods:"GET, POST , PUT , DELETE , PATCH , HEAD",
+//     credentials:true,
+// }
 
 
-app.use(cors(corsOptions))
+app.use(cors())
 
 
 app.use(express.json())
